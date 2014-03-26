0d59d917defdeacca8b726afdf35fafae9b721f8
Daniel Reis <dreis.pt@gmail.com>
2014-03-26 20:52:14 +0000

    Significant refactoring. Added repo pull feature with smart change detection.

--- pipo.py
+++ pipo.py
@@ -11,9 +11,11 @@ It extends pip, providing all it's functions plus the following:
 from __future__ import print_function
 import argparse
 import os
-from os.path import join
+import shutil
 import subprocess
-from subprocess import Popen, PIPE
+import setuptools
+from StringIO import StringIO
+from pprint import pprint
 
 
 SETUP_PY = """\
@@ -25,19 +27,138 @@ setuptools.setup(**setup_data)
 """
 
 
-def get_revno(module_path='.', vcs='bzr'):
-    """ Get VCS revision for the given path """
-    if vcs == 'hg':
-        cmd = "hg log --limit 1 --template {rev} " + module_path
-    elif vcs == 'git':
-        cmd = "git log -1 --oneline --format=format:%at " + module_path
-    else:
-        cmd = 'bzr log --limit=1 --line ' + module_path
-
-    p = Popen(cmd.split(' '), stdout=PIPE, stderr=PIPE)
+def subprocess_call(cmd, path=None):
+    """
+    Runs a shell command and returns it's stdout output.
+    If a path is given, changes to that directory beforehand.
+    """
+    if path:
+        init_path = os.path.abspath(os.getcwd())
+        os.chdir(path)
+    # out = subprocess.call(command.split(' '))
+    p = subprocess.Popen(
+        cmd.split(' '),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
     out, err = p.communicate()
+    if path:
+        os.chdir(init_path)
+    if err:
+        print("ERROR: %s" % err)
+    return out
+
+
+def get_new_revno(path, vcs):
+    """
+    Get VCS revision number for a path.
+    Supports bzr, hg and git.
+     """
+    if vcs == 'hg':
+        cmd = "hg log --limit 1 --template {rev}"  # + path
+    elif vcs == 'git':
+        cmd = "git log -1 --oneline --format=format:%at"
+    else:
+        cmd = 'bzr log --limit=1 --line ./'  # + path
+
+    out = subprocess_call(cmd, path)
     revno = out.strip().split(':')[0]
     return revno or 0
+
+
+def pretty_format(obj):
+    """
+    Return a pretty-printed string representation of an object
+    """
+    f = StringIO()
+    pprint(obj, f)
+    res = f.getvalue()
+    f.close()
+    return res
+
+
+##########
+# info file handling
+##########
+
+_info_filename = '.pipo-info'
+
+
+def write_info(path, module_revno, repo_revno):
+    info = {
+        'module_revno': module_revno,
+        'repo_revno': repo_revno,
+    }
+    open(os.path.join(path, _info_filename), 'w').write(
+        pretty_format(info))
+
+
+def read_info(path):
+    # TODO: use some for of safe eval
+    res = {}
+    info_path = os.path.join(path, _info_filename)
+    if os.path.exists(info_path):
+        res = eval(open(info_path).read())
+    return res or {}
+
+
+##########
+
+
+def pull_repo(path, vcs):
+    """
+    Pull (update) vcs controlled repository.
+    Returns the subprocess exit code.
+    """
+    if os.path.exists(path):
+        print("==== Pulling %s %s ====" % (vcs, path))
+        initial_cwd = os.path.abspath(os.getcwd())
+        params = [vcs, 'pull']
+        os.chdir(path)
+        res = subprocess.call(params)
+        os.chdir(initial_cwd)
+        return res
+
+
+def vcs_detect(path):
+    """
+    Check the vcs for a path, if it's a repo root.
+    """
+    vcs_dirs = ['.bzr', '.git', '.hg']
+    vcss = [x[1:] for x in os.listdir(path) if x in vcs_dirs]
+    return vcss and vcss[0]
+
+
+def get_path_modules(path, pull=False, repo_vcs=None, repo_revno=None):
+    """
+    Discover all OpenERP modules under a path.
+    Returns a list of tuples (path-to-module, module-name, vcs, repo-revno)
+    """
+    # Handle only directories
+    if not os.path.isdir(path):
+        return []
+    # Avoid problems with paths ending with '/'
+    if not os.path.split(path)[1]:
+        path = os.path.split(path)[0]
+    # Check if this is the root of a vcs repo
+    vcs = vcs_detect(path)
+    if vcs:
+        if pull:
+            pull_repo(path, vcs)
+        repo_revno = get_new_revno(path, vcs)
+        repo_vcs = vcs
+
+    res = []
+    if ('__openerp__.py' in os.listdir(path)
+            or os.path.basename(path) == 'server'):
+        res = [os.path.split(path) + (repo_vcs, repo_revno)]
+    else:
+        subdirs = [
+            os.path.join(path, x) for x in sorted(os.listdir(path))
+            if not(x.startswith('.') or '_unported_' in x)]
+        for d in subdirs:
+            res.extend(get_path_modules(
+                d, pull=pull, repo_vcs=repo_vcs, repo_revno=repo_revno))
+    return res
 
 
 def get_package_data(dir):
@@ -71,17 +192,6 @@ def get_manifest_lines(dir):
     return '\n'.join(lines)
 
 
-def _pprint(obj):
-    """ Return a pretty-printed string representation of an object """
-    from StringIO import StringIO
-    from pprint import pprint
-    f = StringIO()
-    pprint(obj, f)
-    res = f.getvalue()
-    f.close()
-    return res
-
-
 def _get_pkgname(name):
     return ('openerp-' + name).replace('_', '-')
 
@@ -91,45 +201,10 @@ def _get_modname(path):
             os.path.basename(os.path.dirname(path)))
 
 
-def _list_modules(parent_path):  # todo: deprecate
-    vcs_dirs = ['.bzr', '.git', '.hg']
-    vcs = 'bzr'
-    res = []
-    if os.path.isdir(parent_path):
-        vcs_dir = [x for x in vcs_dirs if x in os.listdir(parent_path)]
-        if vcs_dir:
-            vcs = vcs_dir[0][1:]
-
-        if os.path.exists(join(parent_path, '__openerp__.py')):
-            return [(parent_path, vcs)]
-        for curdir, dirs, files in os.walk(parent_path):
-            vcs_dir = [x for x in vcs_dirs if x in os.listdir(curdir)]
-            if vcs_dir:
-                vcs = vcs_dir[0][1:]
-            if os.path.exists(join(curdir, '__openerp__.py')):
-                res.append((curdir, vcs))
-    return sorted(res)
-
-
-def _shell_run(path, command):
-    # import subprocess
-    init_path = os.path.abspath(os.getcwd())
-    os.chdir(path)
-    subprocess.call(command.split(' '))
-    os.chdir(init_path)
-
-
-def setup(mod_dir, vcs=None, series='7.0', force=True, cli=False):
+def setup(mod_dir, revno=None, series='7.0', force=True, cli=False):
     """\
     Add the setup.py file to an OpenERP module.
     """
-    try:
-        import setuptools
-    except ImportError:
-        print("\nERROR: setuptools not available. "
-              "You should 'pip install setuptools'.")
-        return False
-
     # Check directory exists
     if not os.path.isdir(mod_dir):
         if cli:
@@ -147,25 +222,6 @@ def setup(mod_dir, vcs=None, series='7.0', force=True, cli=False):
     if cli:
         print("* Generating setup.py for %s" % mod_dotpath)
 
-    mod_dir = os.path.abspath(mod_dir)
-    os.chdir(mod_dir)
-
-    # Check vcs revision number
-    # Unless forced, exit if revision number hasn't changed
-    if vcs:
-        revno = get_revno(mod_dir, vcs)
-        if not force:
-            try:
-                revno_file = os.path.join(mod_dir, 'revno.txt')
-                old_revno = open(revno_file, 'r').read()
-            except IOError:
-                old_revno = 0
-            if revno == old_revno:
-                return False  # "no changes."
-            else:
-                print("%s: %s -> %s" % (mod_dir, old_revno, revno))
-        open('revno.txt', 'w').write(revno)
-
     # prepare data for setuptools
     packages = ([mod_dotpath] +
                 [mod_dotpath + '.%s' % x
@@ -173,7 +229,7 @@ def setup(mod_dir, vcs=None, series='7.0', force=True, cli=False):
     package_data = get_package_data(mod_dir)
 
     # TODO: use safe eval
-    manif = eval(open(join(mod_dir, '__openerp__.py')).read())
+    manif = eval(open(os.path.join(mod_dir, '__openerp__.py')).read())
     if not manif.get('installable', True):
         return False  # "not installable."
 
@@ -195,26 +251,24 @@ def setup(mod_dir, vcs=None, series='7.0', force=True, cli=False):
         'package_data': {'openerp.addons.' + mod_name: package_data},
         'install_requires': manif_depends, }
 
-    setup_data_text = _pprint(setup_data)
+    setup_data_text = pretty_format(setup_data)
     with open(os.path.join(mod_dir, 'setup.py'), 'w') as f:
         f.write(SETUP_PY % (setup_data_text))
 
     # Create README avoiding build warnings
     readme = manif.get('description', '')  # .encode('UTF-8')
-    open(join(mod_dir, 'README.rst'), 'w').write(readme)
+    open(os.path.join(mod_dir, 'README.rst'), 'w').write(readme)
     # Build Manifest
-    open(join(mod_dir, 'MANIFEST.in'), 'w').write(get_manifest_lines(mod_dir))
-    #os.chdir(cwd)
+    open(os.path.join(mod_dir, 'MANIFEST.in'), 'w').write(
+        get_manifest_lines(mod_dir))
     return True
 
 
-def build(path, dist_dir, force=False, cli=False):
-    """\
+def build(path, dist_dir, force=False, pull=False, cli=False):
+    """
     Discover all OpenERP modules under a directory, build their packages and
     place them in a distribution directory.
     """
-    import shutil
-
     dist_dir = dist_dir and os.path.abspath(dist_dir)
     if cli:
         print("\n--------")
@@ -222,35 +276,52 @@ def build(path, dist_dir, force=False, cli=False):
         print("* Target dir is %s" % os.path.abspath(path))
         print("* Dist dir is %s" % dist_dir)
         print("--------")
-    for mod_dir, vcs in _list_modules(os.path.abspath(path)):
+
+    modules = get_path_modules(path, pull=pull)
+    for dirname, module, vcs, repo_revno in modules:
         if cli:
-            print("* %s %s:" % (vcs, os.path.basename(mod_dir)), end=" ")
+            print("* %s %s %s" % (vcs, dirname, module), end=" ")
         # Generate setup.py
-        if setup(mod_dir, vcs, force=force, cli=False):
-            # Call setup.py
-            _shell_run(mod_dir, 'python setup.py --quiet sdist')
-            ###os.chdir(mod_dir)
-            ###subprocess.call(['python', 'setup.py', '--quiet', 'sdist'])
-            # Move distribution to final location
-            if dist_dir:
-                for x in os.listdir(join(mod_dir, 'dist')):
-                    print("%s -> %s" % (dist_dir, x))
-                    shutil.move(join(mod_dir, 'dist', x), join(dist_dir, x))
-        else:
+        mod_dir = os.path.join(dirname, module)
+        mod_info = read_info(mod_dir)
+
+        last_repo_revno = mod_info.get('repo_revno', 0)
+        if not force and last_repo_revno == repo_revno:
+            # print("repo unchanged")
             print(".")
-    # print 'DONE.'
+            continue
 
+        revno = get_new_revno(mod_dir, vcs)
+        last_revno = mod_info.get('module_revno')
+        if last_revno and last_revno == revno and not force:
+            # print("module unchanged")
+            print(".")
+            continue
 
-def _run_shell(cmd):
-    from subprocess import Popen
-    print(' '.join(cmd))
-    p = Popen(cmd)
-    p.communicate()
+        if '__openerp__.py' in os.listdir(mod_dir):
+            installable = setup(mod_dir, revno=revno, cli=False)
+            if not force and not installable:
+                # print("not installable")
+                print(".")
+                continue
+
+        try:
+            subprocess_call('python setup.py --quiet sdist', mod_dir)
+            write_info(mod_dir, revno, repo_revno)
+        except:
+            pass
+        # Move distribution to final location
+        if dist_dir:
+            for x in os.listdir(os.path.join(mod_dir, 'dist')):
+                print("BUILT %s" % x)
+                shutil.move(
+                    os.path.join(mod_dir, 'dist', x),
+                    os.path.join(dist_dir, x))
 
 
 def create(name):
-    _run_shell(['virtualenv', '--system-site-packages', name])
-    _run_shell(['createdb', name])
+    subprocess_call('virtualenv --system-site-packages %s' % name)
+    subprocess_call('createdb %s' % name)
 
 
 def pip(command, *args):
@@ -259,7 +330,7 @@ def pip(command, *args):
         extra = ['http://openerpapps.info/simple/openerp/7.0']
         cmd.extend(['--extra-index-url', ','.join(extra)])
     cmd.extend(list(args[0]))
-    _run_shell(cmd)
+    subprocess_call(' '.join(cmd))
 
 
 if __name__ == "__main__":
@@ -275,12 +346,19 @@ if __name__ == "__main__":
     parser_build.add_argument(
         "-f", "--force", action="store_true",
         help="Force build even if module has no changes")
+    parser_build.add_argument(
+        "-p", "--pull", action="store_true",
+        help="Don't pull repository changes")
 
     args = parser.parse_args()
-    print(args)
     if args.subparser_name == "setup":
         setup(args.mod_dir, cli=True)
     elif args.subparser_name == "build":
-        build(args.path, args.dist_dir, force=args.force, cli=True)
+        build(
+            args.path,
+            args.dist_dir,
+            force=args.force,
+            pull=args.pull,
+            cli=True)
     else:
         print("Invalid command.")
